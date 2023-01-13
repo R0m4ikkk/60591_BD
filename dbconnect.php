6f5e8a2de2553f16bd1a4705076af9fc04ce368a
R0m4ikkk <r.efremov86@gmail.com>
2023-01-14 01:15:26 +0500

    добавил енв и попробовал вывести бд

--- dbconnect.php
+++ dbconnect.php
@@ -1,10 +1,20 @@
 <?php
+session_start();
+require __DIR__ . '/vendor/autoload.php'; //загрузка всех установленных библиотек
+use Dotenv\Dotenv;                        //импорт класса Dotenv из пространства имен dotenv
+if (file_exists(__DIR__."/.env"))
+{
+    $dotenv = Dotenv::createImmutable(__DIR__);
+    $dotenv->load(); //все параметры окружения помещаются в массив $_ENV
+    // var_dump($_ENV);
+}
 // подключение к БД
 try {
-    $conn = new PDO("mysql:host=92.246.214.15:5000;dbname=ais_efremov4559_60591erv", 'ais_efremov4559_60591erv', 'xsV9joah7KjIBnfE9U05wA57');
+    $conn = new PDO("mysql:host=92.246.214.15:3306;dbname=$_ENV[dbname]", $_ENV['dbuser'], $_ENV['dbpassword']);
     // set the PDO error mode to exception
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 }
 catch(PDOException $e) {
     echo "Ошибка подключения к БД: " . $e->getMessage(), $e->getCode( );
-    die();}
+    die();
+}
--- index.php
+++ index.php
@@ -1,2 +1,7 @@
 <?php
-require('dbconnect.php');
+require "dbconnect.php";
+$result = $conn->query("SELECT *  FROM orders") ;
+echo "<h2>Avtomobili</h2>";
+while ($row = $result->fetch()) {
+    echo $row['name']."<br>";
+}
