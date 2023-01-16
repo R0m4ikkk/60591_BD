ea866345e4a2e9bbcb335a2754e5ac7791cbba88
R0m4ikkk <r.efremov86@gmail.com>
2023-01-16 22:41:16 +0500

    /

--- index.php
+++ index.php
@@ -1,7 +1,8 @@
 <?php
 require "dbconnect.php";
-$result = $conn->query("SELECT *  FROM orders") ;
+$result = $conn->query("SELECT *  FROM car") ;
 echo "<h2>Avtomobili</h2>";
 while ($row = $result->fetch()) {
-    echo $row['name']."<br>";
+    echo $row['model_name'];
+    echo $row['price_day']."<br>";
 }
