279edc12781906d82e6a948a02892c8e31d528b7
R0m4ikkk <r.efremov86@gmail.com>
2023-03-20 18:34:20 +0500

    mvc первая часть

--- index.php
+++ index.php
@@ -1,4 +1,17 @@
 <?php
+
+use Framework\Request;
+use Framework\Router;
+use Framework\Application;
+date_default_timezone_set('Asia/Yekaterinburg');
+if ( file_exists(dirname(__FILE__).'/vendor/autoload.php') ) {
+    require_once dirname(__FILE__).'/vendor/autoload.php';
+}
+$request = new Request();
+Application::init();
+echo (new Router($request))->getContent();
+exit();
+
 require('dbconnect.php');
 require('components/meta.php');
 require ('components/auth.php');
